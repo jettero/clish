91b9cb2bece7501580abfe6f4a66073ec7da87ac
Paul Miller <jettero@cpan.org>
2014-05-17 11:15:10 -0400

    command class stub

---
+++ lib/Term/ReadLine/CLISH/Command.pm
@@ -0,0 +1,11 @@
+package Term::ReadLine::CLISH::Command;
+
+use Moose;
+use namespace::autoclean;
+use common::sense;
+
+# NOTE: this is just a stub.  use Term::ReadLine::CLISH::Command::Moose instead
+
+__PACKAGE__->meta->make_immutable;
+
+1;
