c5784d6f6ce1e653b5439576eccc3cb359db5a68
Paul Miller <jettero@cpan.org>
2014-05-21 14:20:49 -0400

    whitespace and argument shifting and things

--- example/cmds/ping.pm
+++ example/cmds/ping.pm
@@ -11,10 +11,6 @@ use common::sense;
 command( name => 'ping',
     help => "send icmp echo requests to a host",
     arguments => [
-        required_argument(
-            target => ['validate_ipv4', 'validate_ipv6', 'validate_hostname'],
-            tag_optional=>1, help => "target host for the pings" ),
-
         optional_argument( count => 'Scalar::Util::looks_like_number',
             help => "number of packets to send" ),
 
@@ -22,41 +18,45 @@ command( name => 'ping',
             help => "size of the packets in bytes" ),
 
         optional_argument( df => undef, help => "set the don't fragment bit" ),
+
+        required_argument(
+            target => ['validate_ipv4', 'validate_ipv6', 'validate_hostname'],
+            tag_optional=>1, help => "target host for the pings" ),
     ],
 );
 
 __PACKAGE__->meta->make_immutable;
 
 sub exec {
-   my $target = shift; # this is validated already
-   my %opts => shift;
+    my $target = shift; # this is validated already
+    my %opts => shift;
 
-   my @args;
-   push @args, -c => $opts{count} if defined $opts{count};
-   push @args, -s => $opts{size}  if defined $opts{size};
-   push @args, -M => "dont"       if defined $opts{df};
+    my @args;
+    push @args, -c => $opts{count} if defined $opts{count};
+    push @args, -s => $opts{size}  if defined $opts{size};
+    push @args, -M => "dont"       if defined $opts{df};
 
-   return eval { systemx( ping => $target, @args ); 1};
+    return eval { systemx( ping => $target, @args ); 1};
 }
 
 sub validate_ipv6 {
-   my $arg = shift;
+    my $arg = shift;
 
-   return eval { Net::IP->new($arg) };
+    return eval { Net::IP->new($arg) };
 }
 
 sub validate_ipv4 {
-   my $arg = shift;
+    my $arg = shift;
 
-   # Don't let people ping local NAT things
-   return if $arg =~ m/^10\./;
-   return if $arg =~ m/^192\./;
-   return if $arg =~ m/^172\./;
+    # Don't let people ping local NAT things
+    return if $arg =~ m/^10\./;
+    return if $arg =~ m/^192\./;
+    return if $arg =~ m/^172\./;
 
-   # or this host
-   return if $arg eq "1.2.3.4"; # also naughty
+    # or this host
+    return if $arg eq "1.2.3.4"; # also naughty
 
-   return eval { Net::IP->new($arg) };
+    return eval { Net::IP->new($arg) };
 }
 
 1;
