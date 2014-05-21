e6cc4132272b8e1e1a8410aec78b9208f1c73bd9
Paul Miller <jettero@cpan.org>
2014-05-20 23:41:39 -0400

    stringify commands better

--- lib/Term/ReadLine/CLISH/Command.pm
+++ lib/Term/ReadLine/CLISH/Command.pm
@@ -8,4 +8,20 @@ use common::sense;
 
 __PACKAGE__->meta->make_immutable;
 
+sub BUILD {
+    my $this = shift;
+    my $ref = ref $this;
+
+    eval qq{
+        package $ref;
+        use overload '""' => \\&Term::ReadLine::CLISH::Command::stringify, fallback => 1;
+    };
+}
+
+sub stringify {
+    my $this = shift;
+
+    return "CMD[" . $this->name . "]";
+}
+
 1;
