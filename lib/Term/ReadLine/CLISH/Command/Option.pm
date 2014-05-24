bbfb2aa4ce6d2cad110e1c81ad5e1590a2d5d683
Paul Miller <jettero@cpan.org>
2014-05-24 09:19:25 -0400

    These aren't methods, they're just boring subs (functions), also, names can have ::, also, undef is just sub {1} — by name

--- lib/Term/ReadLine/CLISH/Command/Option.pm
+++ lib/Term/ReadLine/CLISH/Command/Option.pm
@@ -6,16 +6,19 @@ use common::sense;
 use namespace::autoclean;
 use Moose::Util::TypeConstraints;
 
-subtype 'MethodName', as 'Str', where { m/^[\w_][\w\d_]*\z/ };
-subtype 'ChoiceOfMethods', as 'ArrayRef[MethodName]';
-coerce 'ChoiceOfMethods', from 'MethodName', via { [ $_ ] };
+subtype 'FunctionName', as 'Str', where { m/^(?:::|[\w\d_]+)*\z/ };
+subtype 'ChoiceOfFunctions', as 'ArrayRef[FunctionName]';
+coerce 'ChoiceOfFunctions', from 'FunctionName', via { [ $_ ] };
+coerce 'ChoiceOfFunctions', from 'Undef', via { ['Term::ReadLine::CLISH::Command::Option::ACCEPT'] };
 
 has qw(name is ro isa Str default) => "??";
-has qw(validators is ro isa ChoiceOfMethods coerce 1 default), sub { [] };
+has qw(validators is ro isa ChoiceOfFunctions coerce 1 default), sub { [] };
 has qw(required is ro isa Bool default 0);
 has qw(tag_optional is ro isa Bool default 0);
 has qw(help is ro isa Str default ??);
 
 __PACKAGE__->meta->make_immutable;
+
+sub ACCEPT { 1 }
 
 1;
