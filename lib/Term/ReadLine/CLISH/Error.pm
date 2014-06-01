5d1cc5ba373b062feea7421e9adb8ec72fad0405
Paul Miller <jettero@cpan.org>
2014-06-01 15:26:04 -0400

    Eat $@ when we use it to instantiate an object.

--- lib/Term/ReadLine/CLISH/Error.pm
+++ lib/Term/ReadLine/CLISH/Error.pm
@@ -8,7 +8,7 @@ extends 'Term::ReadLine::CLISH::Message';
 
 has qw(+format default) => "%% %C(red)ERROR%C %s";
 has qw(+msg default) => sub {
-    my $e = $@;
+    my $e = $@; undef $@; # we eat $@'s
     $e =~ s/\s+at\s+\(eval\s+\d+\)\s+line \d+\.//;
     $e
 };
