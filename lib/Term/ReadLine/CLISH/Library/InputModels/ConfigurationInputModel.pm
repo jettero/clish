18140e56b19fa2ca16ea68350a11c6acfcfdaa51
Paul Miller <jettero@cpan.org>
2014-08-02 08:55:59 -0400

    hrm, this never fires

--- lib/Term/ReadLine/CLISH/Library/InputModels/ConfigurationInputModel.pm
+++ lib/Term/ReadLine/CLISH/Library/InputModels/ConfigurationInputModel.pm
@@ -11,9 +11,13 @@ sub post_exec {
     my $this = shift;
     my ($cmd, $args) = @_;
 
+    wtf "[post_exec] $cmd @{[map{($_=>$args->{$_})} keys %$args]}";
+
     return unless $cmd->can("configuration_slot");
 
     my $slot = $cmd->configuartion_slot;
+
+    wtf "[post_exec] $cmd slot=$slot";
 
     $::CLISH->configuration->set($slot, $cmd, $args);
 }
