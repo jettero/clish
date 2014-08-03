712f5228b97f5d2c8a4b6273dad864b5f15c7dd7
Paul Miller <jettero@cpan.org>
2014-08-03 09:19:58 -0400

    sometimes $@ has an object in it, and then the msg type constraint fails

--- lib/Term/ReadLine/CLISH/Message/Warning.pm
+++ lib/Term/ReadLine/CLISH/Message/Warning.pm
@@ -8,7 +8,7 @@ extends 'Term::ReadLine::CLISH::Message';
 
 has qw(+format default) => "%% %C(yellow)WARNING%C %s";
 has qw(+msg default) => sub {
-    my $e = $@; undef $@; # we eat $@'s
+    my $e = "$@"; undef $@; # we eat $@'s
     $e =~ s/\s+at\s+\(eval\s+\d+\)\s+line \d+\.//;
     $e
 };
